a95d11921f15f75e8a0e1c83184921a4f5caa7cb
Mark Steven <aysonsteven@gmail.com>
2019-11-06 10:29:29 +0800

    initial

---
+++ src/main/java/jar/security/constants/Constants.java
@@ -0,0 +1,7 @@
+package jar.security.constants;
+public class Constants {
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "$2a$04$If3ZCfzmlH1zkGDEIYObPOJjCbQYdAP9/ESswB2D2FBqFw59cykRO";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}

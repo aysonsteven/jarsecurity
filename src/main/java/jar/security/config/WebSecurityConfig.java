2a851e6d5ac272d1f847978feb85c3fc4cf00661
Mark Steven <aysonsteven@gmail.com>
2019-11-07 13:21:02 +0800

    work on websecurity config

--- src/main/java/jar/security/config/WebSecurityConfig.java
+++ src/main/java/jar/security/config/WebSecurityConfig.java
@@ -50,9 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/test/*").permitAll()
-                .anyRequest().authenticated()
-                .antMatchers("/auth/login").permitAll()
+                .antMatchers("/test/*","/auth/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()

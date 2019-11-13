7c976e260c0cf8f8563a31e0e251433579433550
Mark Steven <aysonsteven@gmail.com>
2019-11-13 14:49:28 +0800

    add whitelist

--- src/main/java/jar/security/config/WebSecurityConfig.java
+++ src/main/java/jar/security/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/test", "/test/*","/auth/login", "/auth/checklogin").permitAll()
+                .antMatchers("/test", "/test/*","/content/**" ,"/auth/login", "/auth/checklogin").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
